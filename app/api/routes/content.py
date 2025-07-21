92010c3ffdfc865006749df5493892c1654bab50
HerickSilva9 <hericksilva9@outlook.com>
2025-07-21 13:56:04 -0300

    refactor: centraliza 'prefix' e 'tags' no router e faz ajustes de estilo

--- app/api/routes/content.py
+++ app/api/routes/content.py
@@ -4,46 +4,26 @@ from app.schemas.content import ContentCreate, ContentResponse, ContentUpdate
 from app.api.dependencies import SessionDep
 from app.services import content
 
-router = APIRouter()
+router = APIRouter(prefix='/content', tags=['Conteúdo'])
 
 
-@router.post('/create-content/', 
-                     response_model=ContentResponse, 
-                     tags=['Conteúdo'],
-                     summary='Criar um novo conteúdo'
-)
-def create_content(
-    new_content_data: ContentCreate,
-    db: SessionDep
-):
+@router.post('/create/', response_model=ContentResponse)
+def create_content(new_content_data: ContentCreate, db: SessionDep):
     return content.create_content(new_content_data, db)
 
 
-@router.get('/list-contents/',
-                    response_model=list[ContentResponse],
-                    tags=['Conteúdo'],
-                    summary='Listar conteúdos'
-)
+@router.get('/list/', response_model=list[ContentResponse])
 def list_contents(db: SessionDep):
     return content.list_contents(db)
 
 
-@router.put('/update-content/{content_id}', 
-                    response_model=ContentResponse, 
-                    tags=['Conteúdo'],
-                    summary='Atualizar um conteúdo'
-)
-def update_content(content_id: int, 
-                   content_update_data: ContentUpdate,
-                   db: SessionDep
-):
+@router.put('/update/{content_id}', response_model=ContentResponse)
+def update_content(
+        content_id: int, content_update_data: ContentUpdate, db: SessionDep
+        ):
     return content.update_content(content_id, content_update_data, db)
 
 
-@router.delete('/delete/{content_id}',
-                       response_model=ContentResponse,
-                       tags=['Conteúdo'],
-                       summary='Deletar um conteúdo'
-)
+@router.delete('/delete/{content_id}', response_model=ContentResponse)
 def delete_content(content_id: int, db: SessionDep):
     return content.delete_content(content_id, db)
--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -4,49 +4,24 @@ from app.schemas.user import UserCreate, UserResponse, UserUpdate
 from app.api.dependencies import SessionDep
 from app.services import user
 
-router = APIRouter()
+router = APIRouter(prefix='/user', tags=['Usuários'])
 
 
-@router.post('/new-user/', 
-                response_model=UserResponse, 
-                tags=['Usuários'],
-                summary='Adicionar um novo usuário'
-)
-def create_user(
-    new_user_data: UserCreate,
-    db: SessionDep
-):
+@router.post('/new/', response_model=UserResponse)
+def create_user(new_user_data: UserCreate, db: SessionDep):
     return user.create_user(new_user_data, db)
 
 
-@router.get('/list-users/', 
-                 response_model=list[UserResponse], 
-                 tags=['Usuários'],
-                 summary='Listar usuários'
-)
+@router.get('/list/', response_model=list[UserResponse])
 def list_users(db: SessionDep):
     return user.list_users(db)
 
 
-@router.put('/update-user/{user_id}/', 
-                 response_model=UserResponse, 
-                 tags=['Usuários'],
-                 summary='Atualizar dados do usuário'
-)
-def update_user(
-    user_id: int,
-    user_update_data: UserUpdate,
-    db: SessionDep
-):
+@router.put('/update/{user_id}/', response_model=UserResponse)
+def update_user(user_id: int, user_update_data: UserUpdate, db: SessionDep):
     return user.update_user(user_id, user_update_data, db)
 
 
-@router.delete('/delete-user/{user_id}', 
-                    response_model=UserResponse, 
-                    tags=['Usuários'],
-                    summary='Deletar um usuário'
-)
-def delete_user(user_id: int, 
-                db: SessionDep
-):
+@router.delete('/delete/{user_id}', response_model=UserResponse)
+def delete_user(user_id: int, db: SessionDep):
     return user.delete_user(user_id, db)
