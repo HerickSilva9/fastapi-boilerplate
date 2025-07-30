37e69d6820919a28a50e30bfb4d0e0046e0ded2e
HerickSilva9 <hericksilva9@outlook.com>
2025-07-30 17:53:09 -0300

    Definir variáveis de ambiente e implementar no projeto.

--- app/core/db_provider.py
+++ app/core/db_provider.py
@@ -3,12 +3,10 @@ from sqlalchemy import create_engine
 
 from app.models import Base
 from app.models import user, content # force import of models to create tables
+from app.core import settings
 
-# Database URL
-DATABASE_URL = f'sqlite:///app/core/data.db'
-
-# Criar engine (banco SQLite)
-engine = create_engine(DATABASE_URL, echo=False)
+# Create engine
+engine = create_engine(settings.DATABASE_URL, echo=False)
 
 Base.metadata.create_all(engine)  # Cria as tabelas se não existirem
 
--- app/core/settings.py
+++ app/core/settings.py
@@ -1,1 +1,10 @@
 """Settings and Environment Variables"""
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+all_cors_origins = str(os.getenv('BACKEND_CORS_ORIGINS')).split(',')
