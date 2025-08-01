4fa8598929e3968606ffbc31f5dd41609e2e8b4d
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2025-08-01 00:26:40 -0300

    Chore/environment variables (#9)
    * Adicionar dependência 'python-dotenv'
    
    * Adicionar .env aos arquivos ignorados e criar um arquivo de exemplo.
    
    * Definir variáveis de ambiente e implementar no projeto.
    
    * Adicionar comentário ao 'example.env'
    
    * Adicionar dependência para banco Postgresql.

--- app/main.py
+++ app/main.py
@@ -2,16 +2,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from app.api.main import api_router
+from app.core import settings
 
 app = FastAPI()
 
 app.include_router(api_router)
 
-# origins = [""] # Lista de origens permitidas
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.all_cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
