c0a6efbbe3eadc01a30e5146939e267450387776
HerickSilva9 <hericksilva9@outlook.com>
2025-08-02 21:32:31 -0300

    Adiciona handlers globais de erros customizados.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app.api.main import api_router
 from app.core import settings
+from app.exceptions.handlers import (
+    http_exception_handler, generic_exception_handler
+)
 
 app = FastAPI()
+
+app.add_exception_handler(StarletteHTTPException, http_exception_handler)
+app.add_exception_handler(Exception, generic_exception_handler)
 
 app.include_router(api_router)
 
