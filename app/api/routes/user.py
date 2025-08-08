4f63c532a0637723ee6268a79292f54e2db3270a
HerickSilva9 <hericksilva9@outlook.com>
2025-08-08 18:49:18 -0300

    Mudança na ordem dos parâmetros das rotas para maior consistência.

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix='/user', tags=['Usuários'])
 
 
 @router.post('/new/', response_model=UserResponse)
-def create_user(new_user_data: UserCreate, db: SessionDep):
+def create_user(db: SessionDep, new_user_data: UserCreate):
     return user.create_user(db, new_user_data)
 
 
@@ -18,10 +18,10 @@ def get_user_by_id(db: SessionDep, user_id: int):
 
 
 @router.put('/update/{user_id}/', response_model=UserResponse)
-def update_user(user_id: int, user_update_data: UserUpdate, db: SessionDep):
+def update_user(db: SessionDep, user_id: int, user_update_data: UserUpdate):
     return user.update_user(db, user_id, user_update_data)
 
 
 @router.delete('/delete/{user_id}', response_model=UserResponse)
-def delete_user(user_id: int, db: SessionDep):
+def delete_user(db: SessionDep, user_id: int):
     return user.delete_user(db, user_id)
